7453cbd625fd2cbf451d811e9d934bc025703f95
K12-K <kalp12122000@gmail.com>
2020-12-22 02:20:04 +0530

    v2

---
+++ app/src/main/java/com/example/biker/MyListServiceAdapter.java
@@ -0,0 +1,4 @@
+package com.example.biker;
+
+public class MyListServiceAdapter {
+}
---
+++ app/src/main/java/com/example/biker/MyListServiceData.java
@@ -0,0 +1,5 @@
+package com.example.biker;
+
+public class MyListServiceData {
+    private String serviceId, servicerName;
+}
